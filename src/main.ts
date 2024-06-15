d05e8570dbb6394a688b3cb505150b6b82ecfee2
João Victor <freitasjoaovictor49@gmail.com>
2024-06-15 18:16:02 -0300

    refactor: removing unecessary template files from vue create

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createHead } from '@unhead/vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import './main.css'
-
 const app = createApp(App)
+const head = createHead()
+
+app.use(head)
 app.use(createPinia())
 app.use(router)
 
