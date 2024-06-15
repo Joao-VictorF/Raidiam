66dea5d072bec3b5ea8857351e72111d729ea439
João Victor <freitasjoaovictor49@gmail.com>
2024-06-15 17:59:27 -0300

    chore: vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080
+  },
   plugins: [
-    vue(),
+    Vue(),
+
+    // AutoImports Components
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: 'src/components.d.ts'
+    }),
+
+    // AutoImports Directives
+    AutoImport({
+      imports: ['vue', 'vue-router', '@vueuse/core']
+    })
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
