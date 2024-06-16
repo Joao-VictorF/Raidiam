12578d5543242e345fb2a877a4b2ff850ff54011
João Victor <freitasjoaovictor49@gmail.com>
2024-06-16 12:52:00 -0300

    feat(components): adding loading card skeleton component

---
+++ src/components/LoadingCardSkeleton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  showTitle?: boolean
+  showSubtitle?: boolean
+  showText?: boolean
+  showBtn?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  showTitle: true,
+  showSubtitle: true,
+  showText: true,
+  showBtn: false
+})
+</script>
+
+<template>
+  <div class="bg-white w-full rounded-lg shadow-md px-6 py-8">
+    <div class="animate-pulse">
+      <!-- Title -->
+      <div v-if="showTitle" class="h-6 bg-gray-300 rounded w-1/2 mb-4"></div>
+
+      <!-- Subtitle -->
+      <div v-if="showSubtitle" class="h-4 bg-gray-300 rounded w-1/3 mb-2"></div>
+
+      <!-- Text -->
+      <div v-if="showText">
+        <div class="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+        <div class="h-4 bg-gray-300 rounded w-3/5 mb-2"></div>
+        <div class="h-4 bg-gray-300 rounded w-2/3"></div>
+      </div>
+
+      <!-- Button -->
+      <div v-if="showBtn" class="mt-4 h-10 bg-gray-300 rounded w-full"></div>
+    </div>
+  </div>
+</template>
