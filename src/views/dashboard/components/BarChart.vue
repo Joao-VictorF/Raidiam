fe7a0528ef510f2ef306feda2a82193d2278b76f
João Victor <freitasjoaovictor49@gmail.com>
2024-06-19 16:43:38 -0300

    feat: adding reusable bar chart component

---
+++ src/views/dashboard/components/BarChart.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { BarChart, useBarChart } from 'vue-chart-3'
+
+interface Props {
+  dataValues: number[]
+  dataLabels: string[]
+  dataColors?: string[]
+}
+
+const props = defineProps<Props>()
+const chartData = computed(() => ({
+  labels: props.dataLabels,
+  datasets: [
+    {
+      data: props.dataValues,
+      borderWidth: 0
+    }
+  ]
+}))
+
+const options = computed(() => ({
+  plugins: {
+    responsive: true,
+    legend: false
+  }
+}))
+
+const { barChartProps } = useBarChart({
+  options,
+  chartData
+})
+</script>
+
+<template>
+  <BarChart v-bind="barChartProps" />
+</template>
